0f2c355301fb15cc0dab69484102a5cafd6f27f7
MollaMohammadIbrahim <mmibrahim.cseru@gmail.com>
2022-09-03 12:12:18 +0600

    Total problem done before submission

--- firstProblem.cpp
+++ firstProblem.cpp
@@ -12,11 +12,30 @@ int moduloExponentiation(int a, int b, int m){
     }
     else return (a*moduloExponentiation((a*a)%m,(b-1)/2,m))%m;
 }
+ int x,y;
+int extendedEuclid(int a,int b){
+   
+    if(b==0){
+        x = 1;
+        y = 0;
+    }
+    else{
+        extendedEuclid(b,(a%b)%b);
+        int temp = x%b;
+        x = y%b;
+        y =( temp - ((a/b)*y)%b)%b;
+    }
+    return x;
+}
+
 int main(){
     int a,b,c,m;
-    cout<<"intput a,b,m"<<endl;
-    cin>>a>>b>>m;
-    int ans1,ans2;
+    cin>>a>>b>>c>>m;
+    int ans1,ans2,ans;
     ans1 = moduloExponentiation(a,b,m);
-    cout<<ans1<<endl;
+
+    ans2 = extendedEuclid(a,m);
+    
+    ans = (ans1*ans2)%m;
+    cout<<ans<<endl;
 }
